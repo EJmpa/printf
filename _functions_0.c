e67f599523b43c6eee6ef875abf41bbff4dfa158
Chukwu Chukwuemeka Jude <111859279+EJmpa@users.noreply.github.com>
2023-01-22 17:55:15 +0100

    First Function

---
+++ _functions_0.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+/**
+ * print_percent - Prints percent symbol
+ * Description: This function prints percent symbol
+ * @s: Character to print
+ * @buffer: Buffer
+ * Return: Always 1
+ */
+int print_percent(char *s, char *buffer)
+{
+	s++;
+	_putchar(buffer, '%');
+	return (1);
+}
+
+/**
+ * print_char - Prints chars
+ * Description: This function prints chars
+ * @args: Argument to print
+ * @buffer: Buffer
+ * Return: Always 1
+ */
+int print_char(va_list args, char *buffer)
+{
+	char s = va_arg(args, int);
+
+	if (s == '\0')
+	{
+		write(1, &s, 1);
+		return (1);
+	}
+	_putchar(buffer, s);
+	return (1);
+}
+
+/**
+ * print_string - Prints a string
+ * Description: This function prints a string
+ * @args: String to print
+ * @buffer: Buffer
+ * Return: length of @args
+ */
+int print_string(va_list args, char *buffer)
+{
+	char *s = va_arg(args, char *);
+	int len, i;
+
+	if (s)
+	{
+		len = strlen(s);
+		for (i = 0; i < len; i++)
+			_putchar(buffer, *(s + i));
+	}
+	else
+	{
+		len = 6;
+		s = "(null)";
+		for (i = 0; i < len; i++)
+			_putchar(buffer, *(s + i));
+	}
+	return (len);
+}
