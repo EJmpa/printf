d9149cd4fa58e558280cc7c8c51571881e2e87b9
Chukwu Chukwuemeka Jude <111859279+EJmpa@users.noreply.github.com>
2023-01-22 17:47:36 +0100

    Create Buffer

---
+++ _putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _putchar - Prints a char
+ * Description: This function prints a char
+ * @buffer: Buffer
+ * @s: Char to print
+ * Return: 1
+ */
+int _putchar(char *buffer, char s)
+{
+	return (buffer_add(buffer, s));
+}
